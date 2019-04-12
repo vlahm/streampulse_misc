dbaf404d139295aa3071485bf1753d5c9dcd5d4d
Mike Vlah <vlahm13@gmail.com>
2019-04-12 15:41:27 -0400

    added plot, fixed some other stuff in nc storms file

--- nhc_comparison/NC_storms.R
+++ nhc_comparison/NC_storms.R
@@ -10,16 +10,19 @@ sp = sp[sp$regionID == 'NC',]
 sp$regionID = NULL
 
 #grab the Eno subset
-subset = sp[sp$siteID == 'Eno',]
+subset = sp[sp$siteID == 'NHC',]
 
 #remove the main dataset from the global environment if youre low on RAM
-# rm(sp)
+rm(sp)
 
 #remove duplicate rows
 subset = subset[! duplicated(subset),]
 
 #convert datetime from string to POSIXct
 subset$dateTimeUTC = as.POSIXct(subset$dateTimeUTC, tz='UTC')
+
+#remove bad data
+subset = subset[subset$flagID != 'Bad Data',]
 
 #get indices of all points that have been flagged with the word "storm".
 #i just flagged all likely storms for Eno 2017 and NHC 2017
@@ -51,6 +54,14 @@ stormdata = tidyr::spread(subset, variable, value)
 
 #see what you're dealing with
 head(stormdata)
+
+#a useless plot, but it verifies that stuff worked
+plot(stormdata$dateTimeUTC, stormdata$DO_mgL, type='l')
+points(stormdata$dateTimeUTC[stormdata$storm == 1],
+    stormdata$DO_mgL[stormdata$storm == 1], col='red', pch=20, cex=0.1)
+
+#save csv
+write.csv(stormdata, '~/Desktop/NHC_stormdata.csv', row.names=FALSE)
 
 #task: think about how to identify and compare pre-post storm data.
 #how should we define the beginning and end of a storm?
