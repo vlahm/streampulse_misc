7286ebe38e09a4926a1a25969e5772c66c48e57d
Mike Vlah <vlahm13@gmail.com>
2019-04-16 14:58:06 -0400

    working on pre post storm metab plot

--- nhc_comparison/NC_storms.R
+++ nhc_comparison/NC_storms.R
@@ -91,6 +91,49 @@ abline(v=stormdaily$date[stormdaily$storm > 0], col='gray', lty=2)
 write.csv(stormdata, '~/Desktop/NHC_stormdata.csv', row.names=FALSE)
 write.csv(stormdaily, '~/Desktop/NHC_stormdaily.csv', row.names=FALSE)
 
+stormdaily = read.csv('~/Downloads/NHC_stormdaily.csv', stringsAsFactors=FALSE,
+    colClasses=c('date'='Date'))
+
+# stormdaily$GPP = any(is.nan(stormdaily$GPP))
+during = as.logical(stormdaily$storm)
+day1pre = lead(during)
+day2pre = lead(during, 2)
+day3pre = lead(during, 3)
+day1post = lag(during)
+day2post = lag(during, 2)
+day3post = lag(during, 3)
+storm_summary = matrix(NA, 5, 7,
+    dimnames=list(c('GPPmn', 'ERmn', 'GPP:ERmn', 'GPPse', 'ERse'),
+    c('day3pre', 'day2pre', 'day1pre', 'during', 'day1post', 'day2post', 'day3post')))
+for(i in 1:ncol(storm_summary)){
+    gppmn = mean(stormdaily$GPP[get(colnames(storm_summary)[i])], na.rm=TRUE)
+    storm_summary[1, i] = gppmn
+    ermn = mean(stormdaily$ER[get(colnames(storm_summary)[i])], na.rm=TRUE)
+    storm_summary[2, i] = ermn
+    storm_summary[3, i] = gppmn / ermn
+    gppsd = sd(stormdaily$GPP[get(colnames(storm_summary)[i])], na.rm=TRUE)
+    storm_summary[4, i] = gppsd
+    ersd = sd(stormdaily$ER[get(colnames(storm_summary)[i])], na.rm=TRUE)
+    storm_summary[5, i] = ersd
+}
+
+error.bars <- function(x, y, upper, lower=upper, cap.length=0.1, horiz=F,...){
+    if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+        stop("One or more vectors is not the same length")
+
+    if(horiz==F) {
+        arrows(x,y+upper, x, y-lower, angle=90, code=3, length=cap.length, ...)
+    } else if (horiz==T) {
+        arrows(x+upper,y, x-lower, y, angle=90, code=3, length=cap.length, ...)
+    }
+}
+
+barplot(storm_summary[1:3,], beside=TRUE, ylim=c(-7, 5))
+s1 = seq(1.5, 3.5, 1)
+# s2 = seq(4, 22, 3)
+xv = c(s1, s1 + 4, s1 + 7, s1 + 10, s1 + 13, s1 + 16, s1 + 19)
+error.bars(xv, c(storm_summary[1:3,]), upper=rep(0, length(xv)),
+    lower=rep(-2, length(xv)), cap.length=0.01)
 #task: think about how to identify and compare pre-post storm data.
 #how should we define the beginning and end of a storm?
 #how much time should we compare on either side?
